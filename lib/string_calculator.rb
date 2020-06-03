5511a8a4ea5f124fda8560cf7a88b8c1a65c53a8
Brad Phillips <bjrp@protonmail.com>
2020-06-03 22:04:14 +0100

    Refactors add method to single expression

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,17 +1,10 @@
 class StringCalculator
 
   def add(string_numbers)
-    return 0 if string_numbers.empty?
 
-    # if string_numbers.length == 1
-    #   string_numbers.to_i
-    # else
-    #   string_numbers[0].to_i + string_numbers[-1].to_i
-    # end
-
-    numbers_array = string_numbers.split(',')
-
-    numbers_array.map! { |num| num.to_i }.sum
+    string_numbers.split(',')
+    .map { |num| num.to_i }
+    .inject(0, :+)
 
   end
 
