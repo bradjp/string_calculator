1d22461542fa1f41cc387f85952a35895d202b1c
Brad Phillips <bjrp@protonmail.com>
2020-06-04 17:16:39 +0100

    All Rubocop corrections implemented

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
+#:nodoc:
 class StringCalculator
   def add(string_numbers)
     arr = []
+
     x = string_numbers.split("\n")
 
     x.each { |el| arr << el.split(',') }
-    arr.flatten!
-    arr.map { |num| num.to_i }
-       .inject(0, :+)
+
+    arr.flatten.map(&:to_i).inject(0, :+)
   end
 end
